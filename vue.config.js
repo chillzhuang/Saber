cd9e884067769fc5f88641a39363647c22cc6daa
smallwei <1634566606@qq.com>
2019-02-25 11:21:53 +0800

    fix vue config

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
 // 基础路径 注意发布之前要先修改这里
-let baseUrl = './'
 let url = 'http://localhost:8800'
 module.exports = {
-  publicPath: baseUrl, // 根据你的实际情况更改这里
   lintOnSave: true,
   productionSourceMap: false,
   chainWebpack: (config) => {
