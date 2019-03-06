f9abf2254bdefba9e96d7b56ced1a1eff386ddc1
smallwei <1634566606@qq.com>
2019-03-06 14:27:36 +0800

    feat new theme

--- src/page/index/top/top-theme.vue
+++ src/page/index/top/top-theme.vue
@@ -44,6 +44,14 @@ export default {
         {
           name: "炫彩主题",
           value: "theme-star"
+        },
+        {
+          name: "iview主题",
+          value: "theme-iview"
+        },
+        {
+          name: "d2主题",
+          value: "theme-d2"
         }
       ]
     };
