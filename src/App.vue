424e8c8d266b3a1edcf38bd31968e11ddcd185cc
smallchill <smallchill@163.com>
2019-04-22 17:17:15 +0800

    :tada: 授权弹框移出lib包，放到App.vue中

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,26 @@ export default {
       duration: 3000,
       type: "success"
     });
+    setTimeout(()=>{
+      this.$notify.success({
+        title: "bladex授权",
+        dangerouslyUseHTMLString: true,
+        message:
+          '感谢您对bladex的支持，本系统后端采用了bladex开发，如果想使用bladex的授权请<a target="_blank" href="https://bladex.vip/#/vip">点击授权</a>',
+        duration: 3000,
+        type: "success"
+      });
+    },500);
+    setTimeout(()=>{
+      this.$notify.success({
+        title: "avuex授权",
+        dangerouslyUseHTMLString: true,
+        message:
+          '感谢您对avuex的支持，本系统后端采用了avuex开发，如果想使用avuex的授权请<a target="_blank" href="https://avuex.avue.top/#/vip">点击授权</a>',
+        duration: 3000,
+        type: "success"
+      });
+    },500);
   },
   methods: {},
   computed: {}
