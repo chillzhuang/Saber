d6e7dd61b57cca1df8bc1a8e11906b0814239dd9
smallchill <smallchill@163.com>
2019-12-23 11:09:38 +0800

    :tada: 2.6.0发布 升级Hoxton.SR1 适配最新架构

--- src/App.vue
+++ src/App.vue
@@ -12,30 +12,6 @@ export default {
   },
   watch: {},
   created() {
-    this.$notify.success({
-      title: "授权信息",
-      dangerouslyUseHTMLString: true,
-      message:
-        '<p>欢迎使用Saber!</p>' +
-        '<p>该系统可用BladeX增强开发，</p>' +
-        '<p>若要商用强烈推荐高度定制的商业化框架，具体授权信息请访问如下地址。️</p>' +
-        '</br>' +
-        '<p>BladeX 授权地址：<a target="_blank" href="https://bladex.vip/#/vip">点击授权</a></p>' +
-        '<p>AvueX 授权地址：<a target="_blank" href="https://avuejs.com/vip">点击授权</a></p>',
-      duration: 20000,
-      type: "success"
-    });
-    setTimeout(()=>{
-      this.$notify.success({
-        title: "开发手册",
-        dangerouslyUseHTMLString: true,
-        message:
-          '<p>后端开发手册：<a target="_blank" href="https://www.kancloud.cn/smallchill/blade">点击查看</a></p>'+
-          '<p>前端开发手册：<a target="_blank" href="https://www.kancloud.cn/smallchill/saber">点击查看</a></p>',
-        duration: 20000,
-        type: "success"
-      });
-    },500);
   },
   methods: {},
   computed: {}
